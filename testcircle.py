3b9ad9d763ce673fefe302216a5330010445c71e
Alexander <144106386+Sanchell1o@users.noreply.github.com>
2023-12-07 13:26:14 +0300

    Update testcircle.py

--- testcircle.py
+++ testcircle.py
@@ -3,7 +3,7 @@ import circle
 class TestCircle(unittest.TestCase):
     def test_positive_area(self):
         res = circle.area(5)
-        self.assertAlmostEqual(res, 78.5)
+        self.assertAlmostEqual(res, 78.5, 1)
 
     def test_zero_area(self):
         res = circle.area(0)
@@ -11,7 +11,7 @@ class TestCircle(unittest.TestCase):
 
     def test_positive_perimeter(self):
         res = circle.perimeter(5)
-        self.assertAlmostEqual(res, 31.4)
+        self.assertAlmostEqual(res, 31.4, 1)
 
     def test_zero_perimeter(self):
         res = circle.perimeter(0)
